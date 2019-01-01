ac52d8d71f6e705a4a5155766dc5312daa56a1d9
Krzysztof Wieckowski <krzysztof.wieckowski@ericsson.com>
2019-01-01 20:49:22 +0100

    Baza danych MySQL

--- shoppingbackend/src/main/java/pj/projekt/backend/config/HibernateConfig.java
+++ shoppingbackend/src/main/java/pj/projekt/backend/config/HibernateConfig.java
@@ -19,10 +19,10 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 public class HibernateConfig {
 
 	// Change the below based on the DBMS you choose
-	private final static String DATABASE_URL = "jdbc:h2:tcp://localhost/~/onlineshopping"; 
-	private final static String DATABASE_DRIVER = "org.h2.Driver";
-	private final static String DATABASE_DIALECT = "org.hibernate.dialect.H2Dialect";
-	private final static String DATABASE_USERNAME = "sa";
+	private final static String DATABASE_URL = "jdbc:mysql://localhost:3306/car_rental"; 
+	private final static String DATABASE_DRIVER = "com.mysql.jdbc.Driver";
+	private final static String DATABASE_DIALECT = "org.hibernate.dialect.MySQLDialect";
+	private final static String DATABASE_USERNAME = "root";
 	private final static String DATABASE_PASSWORD = "";
 
 
